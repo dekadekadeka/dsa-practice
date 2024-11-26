5fca7907a25b98e2514c1080ea7724b65cdac248
Deka Ambia <48366603+dekadekadeka@users.noreply.github.com>
2024-11-26 15:39:45 -0500

    add print linked list

---
+++ js/linked-lists/dv-print-linked-list.js
@@ -0,0 +1,46 @@
+class Node {
+  constructor (data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor () {
+    this.head = null;
+  }
+
+  append(data) {
+    const newNode = new Node(data);
+
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+
+      current.next = newNode;
+    }
+  }
+
+  print() {
+    const result = [];
+    let current = this.head;
+
+    while (current !== null) {
+      result.push(current.data);
+      current = current.next;
+    }
+    console.log(result.join(' -> '));
+  }
+}
+
+// first, create the Linked List
+const linkedList = new LinkedList;
+// second, populate the Linked List
+linkedList.append(35);
+linkedList.append(69);
+linkedList.append(420);
+linkedList.print(); // prints 35 -> 69 -> 420 to the console
