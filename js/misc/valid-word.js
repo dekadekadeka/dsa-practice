51f6a530eadfaadee19db0ceb5d4d495d065559b
Deka Ambia <48366603+dekadekadeka@users.noreply.github.com>
2024-12-24 12:25:19 -0500

    finish valid word

--- js/misc/valid-word.js
+++ js/misc/valid-word.js
@@ -1,31 +1,40 @@
 // https://www.codewars.com/kata/52f3bb2095d6bfeac2002196
 const validWord = (dictionary, word) => {
-  const joinedDict = dictionary.join('');
-  const sortedDict = dictionary.join('').split('').sort();
-  const sortedWord = word.split('').sort();
+  // my solution:
+  // sort by longest to shortest word, and by reverse alphabetical order (ba, ab, a)
+    dictionary.sort((a, b) => { 
+      if (b.length !== a.length) {
+        return b.length - a.length;
+      }
+      
+      return b.localeCompare(a);
+    });
 
-  let difference = sortedDict.filter(x => !sortedWord.includes(x)).concat(sortedWord.filter(x => !sortedDict.includes(x)));
-
-  if (difference.length > 0 || !joinedDict.includes(word)) {
+    // assign the original word to the result
     let result = word;
 
     for (let i = 0; i < dictionary.length; i++) {
-      const matches = result.matchAll(dictionary[i]);
-
-      if (matches !== null) {
-        for (const match of matches) {
-          result = result.replace(match, '');
-        }
+      // while the current word being checked is still in the result...
+      while (result.includes(dictionary[i])) {
+        // remove it
+        result = result.replace(dictionary[i], '');
       }
     }
 
+    // if all words removed from the result were in the dictionary, then they were all valid
     return result.length === 0;
-  }
-
-  return true;
 }
 
-// all of these work
+// the better solution!
+// const validWord = (dictionary, word) => {
+//   // create a new regex that checks the presence of each word at least once, from the beginning
+//   // to the end of the word
+//   const reg = new RegExp(`^(${dictionary.join('|')})+$`);
+//   // now test the actual word
+//   return reg.test(word);
+// }
+
+validWord(['h','hz','htf','hptw','h','hde'], 'hhzhtfhptwhhde'); // return true
 validWord(['a', 'b', 'c', 'd', 'e', 'f'], 'abcdefg'); // return false
 validWord([], 'codewars'); // return false for empty array
 validWord(['ab', 'a', 'bc'], 'abc'); // return true
