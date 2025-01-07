6fc974f40a8f6cd297dc2db89957421a99e406da
Deka Ambia <48366603+dekadekadeka@users.noreply.github.com>
2025-01-06 22:20:25 -0500

    add strings mix

--- js/misc/strings-mix.js
+++ js/misc/strings-mix.js
@@ -15,7 +15,9 @@ const letterCount = str => {
   );
 }
 
-// merge both hashes into a single one sorted by length and then alpha order
+// merge both hashes into a single one, sorted by count in 1st hash, count in 2nd hash, equal count in both,
+// then letter length and then by alpha order
+// when a key exists in both hashes, then pick the one with the larger value
 const hashMerge = (h1, h2) => {
   const hash = {};
 
@@ -24,23 +26,21 @@ const hashMerge = (h1, h2) => {
   allKeys.forEach(key => {
     // set which hash the greater value originally came from,
     // with 3 being for when the count is equal
-    const originalHash = h1[key] > h2[key] ? 1 : h1[key] === h2[key] ? 3 : 2;
+    const originalHash = h1[key] > (h2[key] || 0) ? 1 : h1[key] === h2[key] ? 3 : 2;
     hash[key] = { originalHash, count: Math.max(h1[key] || 0, h2[key] || 0) };
   })
 
-  const boop = Object.fromEntries(
+  return Object.fromEntries(
     Object.entries(hash).sort(([letterA, countA],[letterB, countB]) => {
-      if (countA.originalHash < countB.originalHash) {
-        return countA.originalHash - countB.originalHash;
-      } else if (countA.count === countB.count) {
-        return letterA.charCodeAt(0) - letterB.charCodeAt(0);
-      } else {
+      if (countA.count !== countB.count) {
         return countB.count - countA.count;
       }
+      if (countA.originalHash !== countB.originalHash) {
+        return countA.originalHash - countB.originalHash;
+      } 
+      return letterA.localeCompare(letterB);
     })
   );
-
-  return boop;
 }
 
 // main function
@@ -48,12 +48,16 @@ const mix = (s1, s2) => {
   const s1hash = letterCount(s1);
   const s2hash = letterCount(s2);
 
-  // merge both hashes into a single one, sorted by count in 1st hash, count in 2nd hash, equal count in both,
-  // then letter length and then by alpha order
-  // when a key exists in both hashes, then pick the one with the larger value
   const merged = hashMerge(s1hash, s2hash);
-  console.log(merged);
+
+  let result = [];
+  
+  for (const letter in merged) {
+    result.push(`${merged[letter].originalHash === 3 ? '=' : merged[letter].originalHash}:${letter.repeat(merged[letter].count)}`);
+  }
+
+  return result.join('/');
 }
 
-// mix('Are they here', 'yes, they are here'); // return '2:eeeee/2:yy/=:hh/=:rr'
+mix('Are they here', 'yes, they are here'); // return '2:eeeee/2:yy/=:hh/=:rr'
 mix('looping is fun but dangerous', 'less dangerous than coding'); // return '1:ooo/1:uuu/2:sss/=:nnn/1:ii/2:aa/2:dd/2:ee/=:gg'
