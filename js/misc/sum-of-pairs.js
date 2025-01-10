13c032dd2691bbe3433de6dc6d9b4bf01f491928
Deka Ambia <48366603+dekadekadeka@users.noreply.github.com>
2025-01-10 13:56:38 -0500

    add sum of pairs

--- js/misc/sum-of-pairs.js
+++ js/misc/sum-of-pairs.js
@@ -7,9 +7,9 @@ const sumPairs = (ints, s) => {
   for (let i = 0; i < ints.length; i++) {
     for (let j = 1; j < ints.length; j++) {
       if (ints[i] + ints[j] === s) {
-        // prevent it from adding itself
-        if (i !== j && currentSecondIndex < j) {
-          // check if the current second index is greater
+        // prevent it from adding itself (i !== j)
+        // make sure j is an index after i (i < j)
+        if (i !== j && i < j && (currentSecondIndex === 0 || j <= currentSecondIndex)) {
           first = ints[i];
           second = ints[j];
           currentSecondIndex = j;
@@ -18,8 +18,13 @@ const sumPairs = (ints, s) => {
     }
   }
 
+  // if currentSecondIndex is still 0, meaning there were no matches, return undefined
+  if (currentSecondIndex === 0) return undefined;
+
   return [first, second];
 }
 
 sumPairs([1, 4, 8, 7, 3, 15], 8); // return [1, 7]
 sumPairs([1, -2, 3, 0, -6, 1], -6); // return [0, -6]
+sumPairs([1, 2, 3, 4, 1, 0], 2); // return [1, 1]
+sumPairs([10, 5, 2, 3, 7, 5], 10); // return [3, 7]
