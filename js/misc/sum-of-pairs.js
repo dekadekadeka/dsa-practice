28afc6ccb5523193e4ad2149e9990416282e1208
Deka Ambia <48366603+dekadekadeka@users.noreply.github.com>
2025-01-10 16:11:49 -0500

    add optimized sum of pairs

--- js/misc/sum-of-pairs.js
+++ js/misc/sum-of-pairs.js
@@ -1,5 +1,6 @@
 // https://www.codewars.com/kata/54d81488b981293527000c8f
-const sumPairs = (ints, s) => {
+// my solution
+const sumPairsOriginal = (ints, s) => {
   let first = 0;
   let second = 0;
   let currentSecondIndex = 0;
@@ -23,6 +24,29 @@ const sumPairs = (ints, s) => {
 
   return [first, second];
 }
+
+// optimized solution
+const sumPairs = (ints, s) => {
+  // create a new set, which only allows each element a single time
+  const seen = new Set();
+  
+  for (let num of ints) {
+    // the match is the sum (s) minus the current num being checked (num + match = s)
+    const match = s - num;
+    
+    // if you find the match in the seen set...
+    if (seen.has(match)) {
+      // that means you found the correct pair
+      return [match, num];
+    }
+    
+    // if seen does not have the match for that num yet, add the num
+    seen.add(num);
+  }
+  
+  // if there's no match at all, return undefined
+  return undefined;
+};
 
 sumPairs([1, 4, 8, 7, 3, 15], 8); // return [1, 7]
 sumPairs([1, -2, 3, 0, -6, 1], -6); // return [0, -6]
