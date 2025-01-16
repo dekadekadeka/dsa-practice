3635c8f20223abe41eca3079a6fe10b725094ffd
Deka Ambia <48366603+dekadekadeka@users.noreply.github.com>
2025-01-16 00:03:58 -0500

    progress on gap

--- js/misc/gap-in-primes.js
+++ js/misc/gap-in-primes.js
@@ -1,24 +1,30 @@
 // https://www.codewars.com/kata/561e9c843a2ef5a40c0000a4
 const gap = (g, m, n) => {
-  // create an array with every number in the m - n range,
-  // then filter it down to its prime numbers
-  const primeRange = Array.from({ length: n - (m - 1) }, (_, i) => m + i).filter(a => {
-    for (let i = 2; i <= Math.sqrt(a); i++) {
-      if (a % i === 0) return false;
-    }
-  
-    return true;
-  });
+  let currentPrime = 0;
+  let count = 0;
 
-  for (let i = 0; i <= primeRange.length - 1; i++) {
-    if (primeRange[i + 1] - primeRange[i] === g) {
-      return [primeRange[i], primeRange[i + 1]];
+  for (let i = m; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      if (i % j === 0) {
+        count++;
+      }
     }
+
+    if (count === 2) {
+      if (i - currentPrime === g) {
+        return [currentPrime, i];
+      }
+
+      currentPrime = i;
+    }
+
+    count = 0;
   }
 
   return null;
 }
 
+gap(1, 30000, 100000); // timing out
 gap(2,100,110); // return [101, 103]
 gap(4,100,110); // return [103, 107]
 gap(6,100,110); // return null
