2e8ee6b916cd89d1583a7f92b140273d8523347c
Deka Ambia <48366603+dekadekadeka@users.noreply.github.com>
2025-01-19 00:08:09 -0500

    add unique in order

---
+++ js/misc/unique-in-order.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/54e6533c92449cc251001667
+const uniqueInOrder = iterable => {
+  if (iterable.length === 0) return [];
+
+  const result = typeof iterable === 'object' ? iterable.join('') : iterable;
+  // regex that matches each character group in order
+  const regex = new RegExp([...new Set(iterable)].map(l => `(${l}+)`).join('|'), 'g');
+  // check if each char is a number; if so, return it as a Number
+  return result.match(regex).map(a => /\d/.test(a) ? Number(a[0]) : a[0]);
+}
+
+uniqueInOrder('AAAABBBCCDAABBB'); // return ['A', 'B', 'C', 'D', 'A', 'B']
+uniqueInOrder('ABBCcAD'); // return ['A', 'B', 'C', 'c', 'A', 'D']
+uniqueInOrder([1,2,2,3,3]); // return [1,2,3]
