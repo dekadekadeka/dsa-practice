158957fbaaa02d5b7e0f6a8584705983bc282ed3
Deka Ambia <48366603+dekadekadeka@users.noreply.github.com>
2025-01-20 19:49:43 -0500

    add minimum steps

--- js/arrays/playing-with-lists-arrays.js
+++ js/arrays/playing-with-lists-arrays.js
@@ -118,3 +118,22 @@ productArray([12,20]); // return [20,12]
 productArray([3,27,4,2]); // return [216,24,162,324]
 productArray([13,10,5,2,9]); // return [900,1170,2340,5850,1300]
 productArray([16,17,4,3,5,2]); // return [2040,1920,8160,10880,6528,16320]
+
+// https://www.codewars.com/kata/5a91a7c5fd8c061367000002
+const minimumSteps = (numbers, value) => {
+  let steps = 0;
+
+  numbers.sort((a, b) => a - b).reduce((acc, curr) => {
+    while (acc < value) {
+      steps++;
+      return acc + curr;
+    }
+  });
+
+  return steps;
+}
+
+minimumSteps([4, 6, 3], 7); // return 1
+minimumSteps([8, 9, 10, 4, 2], 23); // return 3
+minimumSteps([19, 98, 69, 28, 75, 45, 17, 98, 67], 464); // return 8
+minimumSteps([4, 6, 3], 2); // return 0
