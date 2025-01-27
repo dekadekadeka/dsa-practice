e2fb40071993b96cfa65483da1a680d4a0395a5f
Deka Ambia <48366603+dekadekadeka@users.noreply.github.com>
2025-01-27 18:01:43 -0500

    progress on choose best sum

--- js/misc/choose-best-sum.js
+++ js/misc/choose-best-sum.js
@@ -1,5 +1,16 @@
 // https://www.codewars.com/kata/55e7280b40e1c4a06d0000aa
+const calculateTrip = (j, ls, k) => {
+  const result = [];
+
+  for (let m = j; m < j + k; m++) {
+    result.push(ls[m]);
+  }
+
+  return result;
+}
+
 const chooseBestSum = (t, k, ls) => {
+  console.log(t, k, ls)
   if (ls.length < k) return null;
 
   let maxSum = 0;
@@ -9,8 +20,8 @@ const chooseBestSum = (t, k, ls) => {
     let cursor = i + 1;
 
     for (let j = cursor + 1; j <= ls.length - (k - 2); j++) {
-      let trip = ls.slice(cursor, cursor + (k - 1));
-      let sum = [ls[i], ...trip].reduce((a, c) => a + c);
+      let trip = calculateTrip(j, ls, k - 2);
+      let sum = [ls[i], ls[cursor], ...trip].reduce((a, c) => a + c);
 
       if (maxSum < sum && sum <= t) {
         maxSum = sum;
@@ -27,12 +38,14 @@ const chooseBestSum = (t, k, ls) => {
 }
 
 let ts = [91, 74, 73, 85, 73, 81, 87];
-chooseBestSum(331, 2, ts); // return 178
-ts = [50, 55, 57, 58, 60];
-chooseBestSum(174, 3, ts); // return 173
-ts = [50, 55, 56, 57, 58];
-chooseBestSum(163, 3, ts); // return 163
-ts = [50];
-chooseBestSum(163, 3, ts); // return null
-ts = [91, 74, 73, 85, 73, 81, 87];
-chooseBestSum(230, 3, ts); // return 228
+chooseBestSum(331, 4, ts); // return 178
+// let ts = [91, 74, 73, 85, 73, 81, 87];
+// chooseBestSum(331, 2, ts); // return 178
+// ts = [50, 55, 57, 58, 60];
+// chooseBestSum(174, 3, ts); // return 173
+// ts = [50, 55, 56, 57, 58];
+// chooseBestSum(163, 3, ts); // return 163
+// ts = [50];
+// chooseBestSum(163, 3, ts); // return null
+// ts = [91, 74, 73, 85, 73, 81, 87];
+// chooseBestSum(230, 3, ts); // return 228
