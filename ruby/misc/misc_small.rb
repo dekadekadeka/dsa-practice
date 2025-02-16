c4f174518b17c3af6a95006ddd0ad793462b7ea3
Deka Ambia <48366603+dekadekadeka@users.noreply.github.com>
2025-02-15 22:32:21 -0500

    add duty_free to misc_small

--- ruby/misc/misc_small.rb
+++ ruby/misc/misc_small.rb
@@ -9,3 +9,14 @@ end
 
 ordered_count("abracadabra") # return [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
 ordered_count("Code Wars") # return [['C', 1], ['o', 1], ['d', 1], ['e', 1], [' ', 1], ['W', 1], ['a', 1], ['r', 1], ['s', 1]]
+
+# https://www.codewars.com/kata/57e92e91b63b6cbac20001e5
+# Holiday VIII - Duty Free
+def duty_free(price, discount, holiday_cost)
+	price_with_discount = price * (discount * 0.01)
+
+  (holiday_cost / price_with_discount).floor
+end
+
+duty_free(12, 50, 1000) # return 166
+duty_free(17, 10, 500) # return 294
