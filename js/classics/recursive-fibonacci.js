7ffe879160c11e1c72c032dff40b0742752a8d07
Deka Ambia <48366603+dekadekadeka@users.noreply.github.com>
2025-04-10 15:56:24 -0400

    simplify recursive fib

--- js/classics/recursive-fibonacci.js
+++ js/classics/recursive-fibonacci.js
@@ -1,10 +1,8 @@
 // find the fibonacci number at the nth step [1, 1, 2, 3, 5, 8, 13...]
 const fib = n => {
-  if (n <= 1) {
-    return n;
-  } else {
-    return fib(n - 1) + fib(n - 2);
-  }
+  if (n <= 1) return n;
+
+  return fib(n - 1) + fib(n - 2);
 }
 
 // Due to being recursive, it's not as efficient as iterative fib
